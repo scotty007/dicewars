5a0fddc051d44077f682ce7a3b226f1c6a99499e
Scotty <scotty@c-base.org>
2021-01-10 14:24:33 +0100

    Add basic library files

---
+++ dicewars/__version__.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Copyright 2021 Thomas Schott
+#
+# This file is part of dicewars.
+#
+# dicewars is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# dicewars is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with dicewars.  If not, see <http://www.gnu.org/licenses/>.
+
+VERSION = (0, 1, 0, 'dev1')
+__version__ = '.'.join(str(v) for v in VERSION)
---
+++ setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+from pathlib import Path
+
+
+def load_version():
+    version = {}
+    with open(str(here / NAME / '__version__.py'), 'r') as fp:
+        exec(fp.read(), version)
+    return version['__version__']
+
+
+here = Path(__file__).absolute().parent
+
+NAME = 'dicewars'
+VERSION = load_version()
+PACKAGES = find_packages()
+
+setup(
+    name=NAME,
+    version=VERSION,
+    description='Extended Python clone of https://www.gamedesign.jp/games/dicewars/ (backend)',
+    author='Thomas Schott',
+    author_email='scotty@c-base.org',
+    python_requires='>=3.7',
+    packages=PACKAGES,
+)
