edaf9dccd2cc390334d9a7321f4f0dc005f36512
Scotty <scotty@c-base.org>
2021-01-15 23:21:48 +0100

    Add grid cell/area picker methods (for frontend convenience)

--- dicewars/util.py
+++ dicewars/util.py
@@ -44,3 +44,47 @@ def get_player_max_size(grid_areas, player_areas):
         max_size = max(max_size, size)
     assert max_size <= len(player_areas)
     return max_size
+
+
+def point_in_grid_cell(cell, map_x, map_y):
+    # bounding box test
+    (x0, y0), (x1, y1) = cell.bbox
+    if not (x0 <= map_x < x1 and y0 <= map_y < y1):
+        return False
+    # center rect test
+    if cell.border[1][1] <= map_y < cell.border[2][1]:
+        return True
+    x_center = cell.border[0][0]
+    # top triangle test
+    if map_y < cell.border[1][1]:
+        y_edge = (-0.5 if map_x < x_center else 0.5) * (map_x - x_center)
+        return y_edge <= map_y - y0
+    # bottom triangle test
+    y_edge = (0.5 if map_x < x_center else -0.5) * (map_x - x_center)
+    return map_y - y1 < y_edge
+
+
+def point_in_grid_area(grid, area, map_x, map_y):
+    # bounding box test
+    (x0, y0), (x1, y1) = area.bbox
+    if not (x0 <= map_x < x1 and y0 <= map_y < y1):
+        return False
+    # area cells tests
+    for cell_idx in area.cells:
+        if point_in_grid_cell(grid.cells[cell_idx], map_x, map_y):
+            return True
+    return False
+
+
+def pick_grid_cell(grid, map_x, map_y):
+    for cell in grid.cells:
+        if point_in_grid_cell(cell, map_x, map_y):
+            return cell
+    return None
+
+
+def pick_grid_area(grid, map_x, map_y):
+    for area in grid.areas:
+        if point_in_grid_area(grid, area, map_x, map_y):
+            return area
+    return None
